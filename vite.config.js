e8eb1df93e335d97d66c06eb90d2da0f63907459
neerajpandit <pandeyneeraj1507@gmail.com>
2024-09-30 12:13:05 +0530

    upload vercel2aa

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import sitemap from 'vite-plugin-sitemap';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://berrybraintech-website.vercel.app',
-      robots: [
-        {
-          userAgent: '*',
-          allow: '/',
-        },
-      ],
-      outDir: 'dist',
-    })
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the build output goes to the 'dist' folder
+  },
 });
