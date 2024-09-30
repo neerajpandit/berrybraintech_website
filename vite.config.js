bccb3f37398cc5cddba8804b33bde540a7b1c108
neerajpandit <pandeyneeraj1507@gmail.com>
2024-09-30 11:48:32 +0530

    upload vercel2

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import sitemap from 'vite-plugin-sitemap';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://berrybraintech.com', // Your live site URL
-      routes: [
-        '/',
-        '/us',
-        '/service',
-        '/web-development',
-        '/app',
-        '/seo'
+      hostname: 'https://berrybraintech.com',
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
       ],
-    }),
+      outDir: 'dist',
+    })
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  optimizeDeps: {
-    include: ['react-router-dom'],
-  },
 });
