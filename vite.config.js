72dc0983308802d5f6f2a2c90b57d63b59a3c605
neerajpandit <pandeyneeraj1507@gmail.com>
2024-10-02 00:54:29 +0530

    seo

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,38 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://berrybraintech.com',
-      routes: [
-        '/',                    // Home page
-        '/us',                  // Us page
-        '/service',             // Service page
-        '/web-development',     // Web Development page
-        '/app',                 // App page
-        '/seo',                 // SEO page
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist',  // Make sure to point to the correct build output directory
-  },
-});
+  plugins: [react()],
+})
+
+
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import sitemap from 'vite-plugin-sitemap';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     sitemap({
+//       hostname: 'https://berrybraintech.com',
+//       routes: [
+//         '/',                    // Home page
+//         '/us',                  // Us page
+//         '/service',             // Service page
+//         '/web-development',     // Web Development page
+//         '/app',                 // App page
+//         '/seo',                 // SEO page
+//       ],
+//     }),
+//   ],
+//   build: {
+//     outDir: 'dist',  // Make sure to point to the correct build output directory
+//   },
+// });
 
 
 
